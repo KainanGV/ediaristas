ca33f12e4f31a1ef392f4d542f85f69e6aba29cc
KainanGV <71570785+KainanGV@users.noreply.github.com>
2021-06-14 00:37:58 -0300

    estilização com styled components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@styles/Home.module.css';
+import SafeEnvironment from 'ui/components/feedback/safeEnvironment/safeEnvironment';
 
 export default function Home() {
   return (
-      <div>
-        
-      </div>
+    <div>
+      <SafeEnvironment />
+    </div>
   )
 }
