1a7399fc50596f460a019104b7def61a399716fc
KainanGV <71570785+KainanGV@users.noreply.github.com>
2021-06-17 10:19:56 -0300

    card das diaristas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import Head from 'next/head'
 import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
 import SafeEnvironment from 'ui/components/feedback/safeEnvironment/SafeEnvironment';
+import UserInformation from 'ui/components/data-display/UserInformation/UserInformation';
 
 export default function Home() {
   return (
@@ -9,6 +9,8 @@ export default function Home() {
       <PageTitle 
         title={'Conheça os profissionais'} 
         subtitle={'Preencha seu endereço e veja todos os profissionais da sua área'}/>
+
+        <UserInformation name={'Kainan'} picture={'https://github.com/kainangv.png'} rating={3} description={'São Paulo'} />
     </div>
   )
 }
